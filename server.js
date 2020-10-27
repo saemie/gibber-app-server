7263dc79ef1597d0116ff633467dc38fbe02debe
Saemie Chouchane <saemie@sigmaunit.com>
2020-10-27 22:26:48 +0000

    Messy, quick websocket server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const url = require("url");
 const ws = require("ws");
 const minimist = require("minimist");
 const express = require("express");
+var fs = require("fs");
 
 const argv = minimist(process.argv.slice(2), {
     default: {
@@ -14,15 +15,36 @@ const argv = minimist(process.argv.slice(2), {
 const asUrl = url.parse(argv.as_uri);
 const port = asUrl.port;
 
-const options = {}
+const options = {
+    key: fs.readFileSync("keys/server.key"),
+    cert: fs.readFileSync("keys/server.crt"),
+  };
 const app = express();
 
 const server = https.createServer(options, app).listen(port, function () {
-    console.log("Kurento Tutorial started");
     console.log("Open " + url.format(asUrl) + " with a WebRTC capable browser");
 });
+
+// URL for the client to ping: wss://localhost:8443/gibber
 
 const wsServer = new ws.Server({
     server: server,
     path: "/gibber",
+});
+
+
+wsServer.on("connection", function (ws, req) {
+    console.log('Connection established')
+
+    ws.on("error", function (error) {
+        console.error("Connection error", error);
+      });
+    
+      ws.on("close", function () {
+        console.log("Connection closed");
+      });
+    
+      ws.on("message", function (_message) {
+          console.log('_message :>> ', _message);
+      });
 });
