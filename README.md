cca9135b290d64652b220e92a72ffff268d73837
Saemie Chouchane <saemie@sigmaunit.com>
2020-11-01 08:37:49 +0000

    Server with room preparation

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # gibber-app-server
+
+## Keywords / Jargon
+
+- Room. Right now there is only one. Hence, Main Room.
+
+## Single booth prototype
+
+Room gets spun up before the server starts listening for connections.
+Show number of users in the room.
+
+Features:
+
+- Look into the peer-to-peer stun/turn signalling (fun fun... Document it this time!)
+- Session management with cookies etc
+- Broadcaster, choose whether they appoint themselves or automatically appointed
+- Destroy the room, kick users
+
+## Stream List
+
+Features:
+
+- Audio visualiser always on for all.
+- Sound on on list-item hover.
+
+Implementation:
+
+- User gets added to all rooms by default?
--- server.js
+++ server.js
@@ -1,50 +1,104 @@
 const https = require("https");
-const url = require("url"); 
+const url = require("url");
 const ws = require("ws");
 const minimist = require("minimist");
 const express = require("express");
-var fs = require("fs");
+const fs = require("fs");
+const util = require("util");
+
+// Config.
+
+const ROOM_NAME = "room:987245";
+
+// HTTPS and Websocket server setup.
 
 const argv = minimist(process.argv.slice(2), {
-    default: {
-      as_uri: "https://localhost:8443/",
-      ws_uri: "ws://18.133.29.253:8888/kurento",
-    },
-  });
+  default: {
+    as_uri: "https://localhost:8443/",
+    ws_uri: "ws://18.133.29.253:8888/kurento",
+  },
+});
 
 const asUrl = url.parse(argv.as_uri);
 const port = asUrl.port;
 
 const options = {
-    key: fs.readFileSync("keys/server.key"),
-    cert: fs.readFileSync("keys/server.crt"),
-  };
+  key: fs.readFileSync("keys/server.key"),
+  cert: fs.readFileSync("keys/server.crt"),
+};
 const app = express();
 
 const server = https.createServer(options, app).listen(port, function () {
-    console.log("Open " + url.format(asUrl) + " with a WebRTC capable browser");
+  console.log("Open " + url.format(asUrl) + " with a WebRTC capable browser");
 });
 
 // URL for the client to ping: wss://localhost:8443/gibber
 
 const wsServer = new ws.Server({
-    server: server,
-    path: "/gibber",
+  server: server,
+  path: "/gibber",
 });
 
+// Mock api.
+const pauseFor = (time) => {
+    let promise = new Promise(res => setTimeout(res, time));
+    return promise;
+}
 
-wsServer.on("connection", function (ws, req) {
-    console.log('Connection established')
+const createPipeline = async function () {
+    await pauseFor(6000);
+    return 'pipeline:098';
+}
 
-    ws.on("error", function (error) {
-        console.error("Connection error", error);
-      });
-    
-      ws.on("close", function () {
-        console.log("Connection closed");
-      });
-    
-      ws.on("message", function (_message) {
-          console.log('_message :>> ', _message);
-      });
-});
+// Rooms.
+
+const rooms = {};
+
+const initRoom = async (roomId) => {
+    const pipeline = await createPipeline();
+    const sockets = [];
+    const addSocket = (socket) => {
+        const socketIsAlreadyInRoom = sockets.includes(socket);
+        if (socketIsAlreadyInRoom) return;
+        sockets.push(socket);
+        console.log(`User joined ${roomId}`)
+    }
+
+    const getSockets = () => sockets;
+
+    const removeSocket = () => {}
+
+    return { addSocket, getSockets, createEmitter, pipeline, sockets }
+}
+
+const createEmitter = wss => (sockets, message) => sockets.forEach(socket => socket.send(message));
+
+(async () => {
+    console.log('Preparing main room...')
+    const mainRoom = await initRoom(ROOM_NAME);
+    console.log('Main room is ready.')
+    wsServer.on("connection", async socket => {
+        console.log("Connection with a client established");
+        const emit = createEmitter(wsServer);
+        mainRoom.addSocket(socket);
+        const connectedSockets = mainRoom.getSockets();
+        emit(connectedSockets, `Hi from the server to the ${connectedSockets.length} people of room ${ROOM_NAME}`)
+
+        socket.on("error", function (error) {
+            console.error("Connection error", error);
+        });
+        
+        socket.on("close", function () {
+            console.log("Connection closed");
+        });
+        
+        socket.on("message", function (_message) {
+            console.log("_message :>> ", _message);
+            switch (_message) {
+                case 'join':
+                    mainRoom.join(socket)
+                default: return;    
+            }
+        });
+    });
+})();
